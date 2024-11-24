8105ba084b5f5f5c96d53801b345a6965946af19
Olga Mozejko <olga.mozejko@lego.com>
2024-11-24 15:25:48 +0100

    update url for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/a11y-workshop/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
